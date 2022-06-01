51bab6722ebce59c570b13ae5c185a0c05566239
Kevin Roan <103060398+kevinroan69@users.noreply.github.com>
2022-06-01 16:02:41 +0530

    Update on blocks.def.h
    Changed username

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -3,11 +3,11 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/	
 	/*{"", "scripts/dwm_weather.sh",					1000,		0},*/	
-	{" ", "bash /home/drishal/dotfiles/suckless/dwmblocks/scripts/dwm_battery.sh ",					1,		0},
-	{"", "bash /home/drishal/dotfiles/suckless/dwmblocks/scripts/sb-internet",                            1,          0},
-	{"", "bash /home/drishal/dotfiles/suckless/dwmblocks/scripts/cpu.sh",                            1,          0},
-	{"", "bash /home/drishal/dotfiles/suckless/dwmblocks/scripts/memory.sh",	5,		0},
-	{"", "bash /home/drishal/dotfiles/suckless/dwmblocks/scripts/dwm_date.sh",					1,		0},
+	{" ", "bash /home/xtan/dotfiles/suckless/dwmblocks/scripts/dwm_battery.sh ",					1,		0},
+	{"", "bash /home/xtan/dotfiles/suckless/dwmblocks/scripts/sb-internet",                            1,          0},
+	{"", "bash /home/xtan/dotfiles/suckless/dwmblocks/scripts/cpu.sh",                            1,          0},
+	{"", "bash /home/xtan/dotfiles/suckless/dwmblocks/scripts/memory.sh",	5,		0},
+	{"", "bash /home/xtan/dotfiles/suckless/dwmblocks/scripts/dwm_date.sh",					1,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
